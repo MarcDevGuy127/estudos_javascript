671e84d0fa7e650caf625974fcdd7b36cc0942f6
Marcelo Henrique <marceloh.github@gmail.com>
2025-03-01 22:13:31 -0300

    Exercise 2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+//Exercise 1
 console.log("Hello World!");
+
+//Exercise 2
+var a = "Hello";
+var b = 12;
+var c = true;
+
+console.log(a);
+console.log(b); 
+console.log(c);
+/**
+ * var x = 12;
+var y = 9;
+
+console.log(x + y);
+
+FuncCalcular();
+function FuncCalcular(x, y) {
+    var x = 12;
+    var y = 9;
+    return x + y;
+}
+
+console.log(FuncCalcular());
+ */
