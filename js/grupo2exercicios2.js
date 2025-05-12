e0910813d94fb6850e1869438d119e67b9fe67c4
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-12 08:52:11 -0300

    Resolution #1 for grupo2exercicios1

---
+++ js/grupo2exercicios2.js
@@ -0,0 +1,12 @@
+/*
+Grupo 2 - Controle de Fluxo
+    2. Fazer um programa que imprime a tabela ASCII (código decimal, código hexa, caracter) para os códigos de 0 a 127.
+*/
+
+console.log("Tabela ASCII");
+console.log("Dec\tHex\tChar");
+
+for (let i = 0; i <= 127; i++) {
+    //exibe o código decimal, hexadecimal e o caractere correspondente
+    console.log(i + "\t" + i.toString(16).toUpperCase() + "\t" + String.fromCharCode(i));
+}
