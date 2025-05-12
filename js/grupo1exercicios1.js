8d25036de1b1e558e104f092e9ec0d88daccdf91
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-11 21:21:59 -0300

    Adding comments

--- js/grupo1exercicios1.js
+++ js/grupo1exercicios1.js
@@ -3,20 +3,23 @@ Grupo 1 - Sequências Simples
 1. Fazer um programa em JS que pergunta um valor em metros e imprime o correspondente em decímetros, centímetros e milímetros.
 */
 
+//const armazena valores fixos
 const valorMetros = parseFloat(prompt("Digite um valor em metros para converter em decímetros, centímetros e milímetros?"));
 
 function CalcDecimetros(valorMetros) {
-    var decimetros = valorMetros * 10; 
+    let decimetros = valorMetros * 10; 
     return decimetros;
 }
 
 function CalcCentimetros(valorMetros) {
-    var centimetros = valorMetros * 100;
+    let centimetros = valorMetros * 100;
     return centimetros;
 }
 
 function CalcMilimetros(valorMetros) {
-    var milimetros =  valorMetros * 1000;
+    //o escolpo de var é maior que o de let;
+    //o alcance de let é limitado pelo seu bloco. Enquanto var pode ultrapassar todos os blocos do código.
+    let milimetros =  valorMetros * 1000;
     return milimetros;
 }
 
