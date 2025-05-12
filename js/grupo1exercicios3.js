94157ec2ca7c7a8355d87704cea94bf150d2c8d1
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-11 22:58:18 -0300

    Resolution #1for grupo1exercicios3

---
+++ js/grupo1exercicios3.js
@@ -0,0 +1,13 @@
+/*
+Grupo 1 - Sequências Simples
+3. Fazer um programa JS que solicita um número decimal e imprime o correspondente em hexa e octal.
+*/
+
+const valorDecimal = parseFloat(prompt("Digite um valor em decimal:"));
+
+if (valorDecimal % 1 == 0) {
+    console.log(`Hexadecimal: ${valorDecimal.toString(16)}`);
+    console.log(`Octal: ${valorDecimal.toString(8)}`);
+} else {
+    prompt(`O valor ${valorDecimal} não é decimal!`);
+}
