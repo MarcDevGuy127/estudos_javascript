f76e4d075ea18fe6ad982a3fba7018fc91bec716
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-11 23:58:11 -0300

    Resolution #1 for grupo1exercicios4

---
+++ js/grupo1exercicios4.js
@@ -0,0 +1,12 @@
+/*
+Grupo 1 - Sequências Simples
+4. Fazer um programa em JS que pergunte um valor em graus Fahrenheit e
+imprime no vídeo o correspondente em graus Celsius. Usar uma variável
+double para ler o valor em Fahrenheit e a fórmula:
+C = (F - 32.0) * (5.0 / 9.0).*/
+
+const valorFahrenheit = parseFloat(prompt("Digite um valor em Fahrenheit para converter em Celsius"));
+
+const valorCelsius = ((valorFahrenheit - 32.0) * (5.0 / 9.0));
+
+console.log(`${valorFahrenheit}°F => ${valorCelsius}°C`);
