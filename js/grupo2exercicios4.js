83f907579762dd5b369edb88c5b74c2c10ceed54
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-15 18:52:03 -0300

    Resolution #1 for grupo2exercicios4

---
+++ js/grupo2exercicios4.js
@@ -0,0 +1,50 @@
+/*
+Grupo 2 - Controle de Fluxo
+    4. Fazer um programa que lê um valor, um operador (+, -, *, /) e outro valor 
+    e imprime o resultado da expressão: <valor 1> <operador> <valor 2>
+*/
+
+//for (let i = 0; i < 10; i++) {
+for (let i = 0; i < Infinity; i++) {
+    const operador = prompt("Calculadora \n Escolha um operador (+, -, *, /) ou Q:");
+    
+    if (operador == "Q") {
+        alert("Você saiu da calculadora!"); //console.log("Você saiu da calculadora!");
+        break;
+    }
+    if (operador !== "+" && operador !== "-" && operador !== "*" && operador !== "/") {
+        alert("Essa operação é inválida!");
+        break;
+    }
+
+    const valor1 = parseFloat(prompt("Digite um valor:"));
+    const valor2 = parseFloat(prompt("Digite um outro valor:"));
+    let resultado;
+    
+    switch (operador) {
+        case "+":
+            resultado = valor1 + valor2;
+            //console.log(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            alert(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            break;
+        case "-":
+            resultado = valor1 - valor2;
+            //console.log(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            alert(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            break;
+        case "*":
+            resultado = valor1 * valor2;
+            //console.log(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            alert(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            break;
+        case "/":
+            resultado = valor1 / valor2;
+            //console.log(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            alert(`${valor1} ${operador} ${valor2} = ${resultado}`);
+            break;
+        default:
+            //console.log("Operador não reconhecido!");
+            alert("Operador não reconhecido!");
+            break;
+    }
+}
