4ac6477708421dc8366eaa3df945c3c0eec860bb
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-17 23:02:23 -0300

    Resolution #1 for grupo3exercicios2

---
+++ js/grupo3exercicios2.js
@@ -0,0 +1,23 @@
+/*
+Grupo 3 - Funções
+    Fazer uma função que calcula o fatorial de um número. Implementar uma
+    versão recursiva e uma versão com laço.
+*/
+
+function fatorialRecursivo(numero) {
+    if (numero === 0 || numero === 1) {
+        return 1;
+    }
+    return numero * fatorialRecursivo(numero-1);
+}
+
+function fatorialLaco(numero) {
+    let resultado = 1;
+    for (let i = 2; i <= numero; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+const numero = parseFloat(prompt("Digite um número para calcular fatorial:"));
+alert(`Fatorial(Recursivo) de ${numero}!: ${fatorialRecursivo(numero)} \n Fatorial(Laço) de ${numero}!: ${fatorialLaco(numero)}`);
